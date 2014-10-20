a9bb16174c0c041d0eb998d53fa49a7723100898
James Muspratt <jmuspratt@gmail.com>
2014-10-20 15:50:00 -0600

    rename scripts, add read me

---
+++ README.md
@@ -0,0 +1,1 @@
+This demo is based on Andrew Fricke's post, [Rolling your own PJAX implementation with History.js](http://uhnomoli.com/2014/01/10/Rolling-your-own-PJAX-implementation-with-History.js/).
--- js/scripts.js
+++ js/scripts.js
@@ -1,116 +1,113 @@
-// Based on 
-// http://uhnomoli.com/2014/01/10/Rolling-your-own-PJAX-implementation-with-History.js/
+// Adapted from http://uhnomoli.com/2014/01/10/Rolling-your-own-PJAX-implementation-with-History.js/
 
-
-(function ($) {
-	var $document = $(document);
-    var $ajax_links = $('.main-header a');
-	
-	if (!History.enabled) {
-		return false;
+var Hijax = {
+	settings: {
+		content: '#main', // string only, not the jQuery object
+		$window: $(window),
+		$ajax_links: $('.main-header a'),
+		fade_duration: 300,
+	},
+	init: function() {
+		Hijax.bind();
+	},
+	bind: function(){
 		
-	}
-    
-	var root = History.getRootUrl();
-	
-	//
-	// $.expr.filters.internal = function (elem) {
-	// 	return (elem.hostname == window.location.hostname && /(\/|\.html)$/i.test(elem.pathname)) || false;
-	// };
-    
-	function update_nav (clicked_link) {
-		var $clicked_link = $(clicked_link);
-		$ajax_links.closest('nav').find('li').removeClass('active');
-		$clicked_link.closest('li').addClass('active');
-	}
-	
-	function find_all($html, selector) {
-		return $html.filter(selector).add($html.find(selector));
-	}
-    
-	function parse_html(html) {
-		return $($.parseHTML(html, document, true));
-	}
-    
-	function parse_response(html) {
-		var
-		head = /<head[^>]*>([\s\S]+)<\/head>/.exec(html),
-		body = /<body[^>]*>([\s\S]+)<\/body>/.exec(html),
-            
-		$head = head ? parse_html(head[1]) : $(),
-		$body = body ? parse_html(body[1]) : $(),
-            
-		title = $.trim(find_all($head, 'title').last().html()),
-		$content = $.trim(find_all($body, '#content').first().html());
-        
-		return {
-			'title': title,
-			'$content': $content
-		}
-	}
-    
-	
-	
-	
-	
-	
-	$document.ready(function () {
+		var root = History.getRootUrl();
 		
-		$ajax_links.on('click', function (event) {
+		// on click
+		Hijax.settings.$ajax_links.click(function(event) {
+			
 			if (event.which == 2 || event.ctrlKey || event.metaKey) {
 				return true;
 			}
-			update_nav(this);
+			Hijax.update_nav(this);
 			History.pushState(null, null, $(this).attr('href'));
 			
 			event.preventDefault();
 			
 			return false;
 		});
-	});
-    
-	
-	
-	$(window).on('statechange', function () {
-		var
-		url = History.getState().url,
-		rel = url.replace(root, '/');
+		
+		
+		// on window state change
+		Hijax.settings.$window.on('statechange', function () {
+			var
+			url = History.getState().url,
+			rel = url.replace(root, '/');
+			
+			
         
-		$.get(rel).done(function (date) {
-			var response = parse_response(date);
+			$.get(rel).done(function (data) {
+				var response = Hijax.parse_response(data);
             
-			if (!response.$content.length) {
-				document.location.href = url;
+				if (!response.$content.length) {
+					document.location.href = url;
                 
-				return false;
-			}
+					return false;
+				}
             
-			var $content = $('#content');
+				var $content = $(Hijax.settings.content);
             
-			if (response.title.length) {
-				$('title').last().html(response.title);
-			}
+				if (response.title.length) {
+					$('title').last().html(response.title);
+				}
             
-			$content
-			.addClass('exit')
-			.fadeOut(100, function(){
-				$(this).removeClass('exit')
-				
-			})
-			.promise()
-			.done(function () {
 				$content
-				.html(response.$content)
-				.addClass('enter')
-				.fadeIn(300, function(){ 
-					$(this).removeClass('enter')
+				.addClass('exit')
+				.fadeOut(Hijax.settings.fade_duration, function(){
+					$(this).removeClass('exit')
+				
+				})
+				.promise()
+				.done(function () {
+					$content
+					.html(response.$content)
+					.addClass('enter')
+					.fadeIn(Hijax.settings.fade_duration, function(){ 
+						$(this).removeClass('enter')
+					});
 				});
+			}).fail(function () {
+				document.location.href = url;
+				return false;
 			});
-		}).fail(function () {
-			document.location.href = url;
-			return false;
 		});
-	});
+		
+	},
 	
+	update_nav: function(clicked_link) {
+		var $clicked_link = $(clicked_link);
+		Hijax.settings.$ajax_links.closest('nav').find('li').removeClass('active');
+		$clicked_link.closest('li').addClass('active');
+	},
+	find_all: function($html, selector) {
+		return $html.filter(selector).add($html.find(selector));
+		
+	},
+	parse_html: function(html) {
+		return $($.parseHTML(html, document, true));
+		
+	},
+	parse_response: function(html) {
+		var
+		head = /<head[^>]*>([\s\S]+)<\/head>/.exec(html),
+		body = /<body[^>]*>([\s\S]+)<\/body>/.exec(html),
+            
+		$head = head ? Hijax.parse_html(head[1]) : $(),
+		$body = body ? Hijax.parse_html(body[1]) : $(),
+            
+		title	 = $.trim(Hijax.find_all($head, 'title').last().html()),
+		$content = $.trim(Hijax.find_all($body, Hijax.settings.content).first().html());
+        
+		return {
+			'title': title,
+			'$content': $content
+		}
+	},
 	
-})(jQuery);
+};
+
+
+$(document).ready(function() {
+	Hijax.init();
+});
