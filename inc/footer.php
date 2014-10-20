177627712b6db082190401540a29700276deac9a
James Muspratt <jmuspratt@gmail.com>
2014-10-20 15:44:56 -0600

    delete includes

--- inc/footer.php
+++
@@ -1,8 +0,0 @@
-	<!-- js -->
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-	<script src="js/jquery.history.js"></script>
-	<script src="js/scripts-new.js"></script> 
-	
-
-</body>
-</html>
